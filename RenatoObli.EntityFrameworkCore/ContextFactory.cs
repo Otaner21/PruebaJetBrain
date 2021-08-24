fbf30eb216a1122ff2e97f2e6c6d760a65449eef
Otaner21 <renatods2121@gmail.com>
2021-08-24 00:27:24 -0300

    24/08/2021

--- RenatoObli.EntityFrameworkCore/ContextFactory.cs
+++ RenatoObli.EntityFrameworkCore/ContextFactory.cs
@@ -10,7 +10,7 @@ namespace RenatoObli.EntityFrameworkCore
         {
             var builder = new DbContextOptionsBuilder<Context>();
 
-            builder.UseSqlServer(@"Server=DESKTOP-MNVS5C9\SQLEXPRESS;Database=RenatoObliBD;Trusted_Connection=True;");
+            builder.UseSqlServer(@"Server=DESKTOP-MNVS5C9\SQLSERVERLOCAL;Database=RenatoObliBD;Trusted_Connection=True;");
           //  Data Source=DESKTOP-MNVS5C9\SQLEXPRESS;Initial Catalog=RenatoObliBD;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
             return new Context(builder.Options);
         }
